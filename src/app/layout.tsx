292b0d5c58645a562576b74347bfd28d880dac6d
jacobbinnie <hello@jacobbinnie.com>
2023-10-22 09:03:47 -0400

    fix: font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Roboto } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const roboto = Roboto({
+  weight: "400",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Jacob Binnie | Software Engineer",
@@ -19,7 +22,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={roboto.className}>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,31 @@
 "use client";
-import { Roboto } from "next/font/google";
 import { Player } from "@lottiefiles/react-lottie-player";
 import Cube from "./cube.json";
 
-const roboto = Roboto({
-  weight: "400",
-  subsets: ["latin"],
-});
-
 export default function Home() {
   return (
-    <main className={roboto.className}>
-      <div className="bg-slate-200 h-screen flex justify-center items-center w-full tracking-tighter text-black ">
-        <div className="fixed top-0 left-0 p-2 flex justify-end w-full">
-          <div className="flex gap-2">
-            <a href="https://linkedin.com/in/jacobbinnie" target="_blank">
-              Linkedin
-            </a>
-            <a href="https://github.com/jacobbinnie" target="_blank">
-              Github
-            </a>
-          </div>
-        </div>
-
-        <Player autoplay loop src={Cube} style={{ height: "75vh" }} />
-
-        <div className="fixed bottom-0 left-0 p-2 flex justify-between w-full">
-          <h1>Jacob Binnie</h1>
-
-          <div className="flex gap-2">
-            <h2>Software Engineer</h2>
-            <h2>New York City</h2>
-          </div>
+    <div className="bg-slate-200 h-screen flex justify-center items-center w-full tracking-tighter text-black ">
+      <div className="fixed top-0 left-0 p-2 flex justify-end w-full">
+        <div className="flex gap-2">
+          <a href="https://linkedin.com/in/jacobbinnie" target="_blank">
+            Linkedin
+          </a>
+          <a href="https://github.com/jacobbinnie" target="_blank">
+            Github
+          </a>
         </div>
       </div>
-    </main>
+
+      <Player autoplay loop src={Cube} style={{ height: "75vh" }} />
+
+      <div className="fixed bottom-0 left-0 p-2 flex justify-between w-full">
+        <h1>Jacob Binnie</h1>
+
+        <div className="flex gap-2">
+          <h2>Software Engineer</h2>
+          <h2>New York City</h2>
+        </div>
+      </div>
+    </div>
   );
 }
