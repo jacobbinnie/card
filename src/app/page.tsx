e901bb7dddf6e2ed71ce8678e4b41c5a243c330f
jacobbinnie <hello@jacobbinnie.com>
2023-12-01 17:39:59 -0500

    feat: change up styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,9 @@ export default function Home() {
     <div className="bg-slate-200 h-screen flex justify-center items-center w-full tracking-tighter text-black ">
       <div className="fixed top-0 left-0 p-2 flex justify-end w-full">
         <div className="flex gap-2">
+          <a href="https://kiki.club" target="_blank">
+            Kiki
+          </a>
           <a href="https://www.strava.com/athletes/jacobbinnie" target="_blank">
             Strava
           </a>
@@ -22,7 +25,13 @@ export default function Home() {
         </div>
       </div>
 
-      <Player autoplay loop src={Cube} style={{ height: "75vh" }} />
+      <div className="relative w-full max-w-lg">
+        <div className="absolute top-0 -left-4 w-72 h-72 bg-pink-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob" />
+        <div className="absolute top-0 -right-4 w-64 h-64 bg-yellow-400 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000" />
+        <div className="absolute -bottom-8 left-20 w-72 h-72 bg-orange-500 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-4000" />
+        <div className="absolute bottom-0 right-0 w-96 h-96 bg-pink-400 rounded-full mix-blend-multiply filter blur-lg opacity-60 animate-blob animation-delay-3000" />
+        <div className="absolute top-16 -right-8 w-72 h-72 bg-yellow-300 rounded-full mix-blend-multiply filter blur-lg opacity-60 animate-blob animation-delay-5000" />
+      </div>
 
       <div className="fixed bottom-0 left-0 p-2 flex justify-between w-full">
         <h1>Jacob Binnie</h1>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,42 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      animation: {
+        blob: "blob 10s infinite ease-in-out",
+      },
+      keyframes: {
+        blob: {
+          "0%": {
+            transform: "translate(0px, 0px) scale(1) rotate(0deg)",
+          },
+          "25%": {
+            transform: "translate(30px, -50px) scale(1.1) rotate(90deg)",
+          },
+          "50%": {
+            transform: "translate(-20px, 20px) scale(0.9) rotate(180deg)",
+          },
+          "75%": {
+            transform: "translate(0px, 0px) scale(1) rotate(270deg)",
+          },
+          "100%": {
+            transform: "translate(0px, 0px) scale(1) rotate(360deg)",
+          },
+        },
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
