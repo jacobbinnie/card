edb25ab3ba86280b0689f935c2eafec5729566b9
jacobbinnie <hello@jacobbinnie.com>
2023-12-01 18:03:12 -0500

    feat: add changes to colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,10 @@ import Cube from "./cube.json";
 
 export default function Home() {
   return (
-    <div className="bg-slate-200 h-screen flex justify-center items-center w-full tracking-tighter text-black ">
+    <div className="bg-slate-200 h-screen overflow-hidden flex justify-center items-center w-full tracking-tighter text-black ">
       <div className="fixed top-0 left-0 p-2 flex justify-end w-full">
         <div className="flex gap-2">
-          <a href="https://kiki.club" target="_blank">
+          <a href="https://kiki.club/" target="_blank">
             Kiki
           </a>
           <a href="https://www.strava.com/athletes/jacobbinnie" target="_blank">
@@ -25,12 +25,10 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="relative w-full max-w-lg">
-        <div className="absolute top-0 -left-4 w-72 h-72 bg-pink-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob" />
-        <div className="absolute top-0 -right-4 w-64 h-64 bg-yellow-400 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000" />
-        <div className="absolute -bottom-8 left-20 w-72 h-72 bg-orange-500 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-4000" />
-        <div className="absolute bottom-0 right-0 w-96 h-96 bg-pink-400 rounded-full mix-blend-multiply filter blur-lg opacity-60 animate-blob animation-delay-3000" />
-        <div className="absolute top-16 -right-8 w-72 h-72 bg-yellow-300 rounded-full mix-blend-multiply filter blur-lg opacity-60 animate-blob animation-delay-5000" />
+      <div className="relative w-full max-w-2xl">
+        <div className="absolute top-0 right-0 w-72 h-72 bg-pink-300 rounded-full mix-blend-multiply filter blur-lg opacity-60 animate-blob animation-delay-1000" />
+        <div className="absolute bottom-0 left-0 w-96 h-96 bg-pink-200 rounded-full mix-blend-multiply filter blur-lg opacity-60 animate-blob animation-delay-2000" />
+        <div className="absolute top-28 left-8 w-72 h-72 bg-yellow-200 rounded-full mix-blend-multiply filter blur-lg opacity-60 animate-blob animation-delay-5000" />
       </div>
 
       <div className="fixed bottom-0 left-0 p-2 flex justify-between w-full">
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,19 +19,16 @@ const config: Config = {
       keyframes: {
         blob: {
           "0%": {
-            transform: "translate(0px, 0px) scale(1) rotate(0deg)",
+            transform: "translate(0px, 0px) scale(1)",
           },
-          "25%": {
-            transform: "translate(30px, -50px) scale(1.1) rotate(90deg)",
+          "33%": {
+            transform: "translate(30px, -50px) scale(1.1)",
           },
-          "50%": {
-            transform: "translate(-20px, 20px) scale(0.9) rotate(180deg)",
-          },
-          "75%": {
-            transform: "translate(0px, 0px) scale(1) rotate(270deg)",
+          "66%": {
+            transform: "translate(-20px, 20px) scale(0.9)",
           },
           "100%": {
-            transform: "translate(0px, 0px) scale(1) rotate(360deg)",
+            transform: "tranlate(0px, 0px) scale(1)",
           },
         },
       },
