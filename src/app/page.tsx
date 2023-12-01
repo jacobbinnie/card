61a084650f40a370668cd7e4695d59cd5e0b3c6f
jacobbinnie <hello@jacobbinnie.com>
2023-12-01 18:16:32 -0500

    feat: remove blob

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,7 +26,6 @@ export default function Home() {
       </div>
 
       <div className="relative w-full max-w-lg">
-        <div className="absolute top-0 -left-4 w-96 h-96 bg-orange-300 rounded-full mix-blend-multiply filter blur-2xl opacity-70 animate-blob"></div>
         <div className="absolute top-0 -right-4 w-72 h-72 bg-yellow-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000"></div>
         <div className="absolute -bottom-8 left-20 w-72 h-72 bg-pink-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-4000"></div>
       </div>
