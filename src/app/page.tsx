a7372ce8aa9baadc909c7f36a7c1c8eb8309e2d8
jacobbinnie <hello@jacobbinnie.com>
2023-12-01 18:13:43 -0500

    feat: update colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,10 +25,10 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="relative w-full max-w-2xl">
-        <div className="absolute top-0 right-0 w-72 h-72 bg-pink-300 rounded-full mix-blend-multiply filter blur-lg opacity-60 animate-blob animation-delay-1000" />
-        <div className="absolute bottom-0 left-0 w-96 h-96 bg-pink-200 rounded-full mix-blend-multiply filter blur-lg opacity-60 animate-blob animation-delay-2000" />
-        <div className="absolute top-28 left-8 w-72 h-72 bg-yellow-200 rounded-full mix-blend-multiply filter blur-lg opacity-60 animate-blob animation-delay-5000" />
+      <div className="relative w-full max-w-lg">
+        <div className="absolute top-0 -left-4 w-96 h-96 bg-orange-300 rounded-full mix-blend-multiply filter blur-2xl opacity-70 animate-blob"></div>
+        <div className="absolute top-0 -right-4 w-72 h-72 bg-yellow-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000"></div>
+        <div className="absolute -bottom-8 left-20 w-72 h-72 bg-pink-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-4000"></div>
       </div>
 
       <div className="fixed bottom-0 left-0 p-2 flex justify-between w-full">
