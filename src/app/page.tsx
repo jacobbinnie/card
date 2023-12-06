c51979333b94332701e5ee6aae5e2ee7c732564a
Jacob Binnie <83803154+jacobbinnie@users.noreply.github.com>
2023-12-06 16:26:13 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,10 +25,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="relative w-full max-w-lg">
-        <div className="absolute top-0 -right-4 w-72 h-72 bg-yellow-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000"></div>
-        <div className="absolute -bottom-8 left-20 w-72 h-72 bg-pink-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-4000"></div>
-      </div>
+      <Player autoplay loop src={Cube} style={{ height: "75vh" }} />
 
       <div className="fixed bottom-0 left-0 p-2 flex justify-between w-full">
         <h1>Jacob Binnie</h1>
