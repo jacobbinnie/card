ddaa5bb19621eaa5abb5bdd69d1c4b194a2e1f65
jacobbinnie <hello@jacobbinnie.com>
2023-12-10 11:42:16 -0500

    feat: add email link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
           urlString="https://www.shadergradient.co/customize?animate=on&axesHelper=off&bgColor1=%23000000&bgColor2=%23000000&brightness=0.8&cAzimuthAngle=180&cDistance=2.8&cPolarAngle=80&cameraZoom=9.1&color1=%23606080&color2=%238d7dca&color3=%23212121&destination=onCanvas&embedMode=off&envPreset=city&format=gif&fov=45&frameRate=10&gizmoHelper=hide&grain=on&lightType=3d&pixelDensity=1&positionX=0&positionY=0&positionZ=0&range=enabled&rangeEnd=40&rangeStart=0&reflection=0.1&rotationX=50&rotationY=0&rotationZ=-60&shader=defaults&type=waterPlane&uAmplitude=0.4&uDensity=1.5&uFrequency=0&uSpeed=0.2&uStrength=1.8&uTime=8&wireframe=false"
         />
       </ShaderGradientCanvas>
-      <div className="fixed top-0 left-0 p-4 flex justify-end w-full">
+      <div className="fixed top-0 left-0 p-4 flex items-end w-full flex-col gap-2">
         <div className="flex gap-2">
           <a href="https://kiki.club/" target="_blank">
             Kiki
@@ -42,6 +42,14 @@ export default function Home() {
             IG
           </a>
         </div>
+
+        <a
+          className="bg-slate-100 bg-opacity-20 px-2 py-1 rounded-xl"
+          href="mailto:collab@jacobbinnie.com"
+          target="_blank"
+        >
+          Email (business enquiries only)
+        </a>
       </div>
 
       <div className="fixed bottom-0 left-0 p-4 flex justify-between w-full">
